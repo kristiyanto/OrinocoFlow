f6edff2febbf177f240caa27d94716512e469f8e
Daniel Kristiyanto <danielkr@uw.edu>
2016-07-09 01:34:39 -0700

    Anchor up, sail unrolled.

---
+++ README.md
@@ -0,0 +1,109 @@
+# Sail Away!
+By Daniel Kristiyanto
+Seattle. Summmer, 2016
+
+# Challenge Summary
+
+This challenge requires participats to:
+
+Use Venmo payments that stream in to build a graph of users and their relationship with one another.
+
+Calculate the median degree of a vertex in a graph and update this each time a new Venmo payment appears. You will be calculating the median degree across a 60-second sliding window.
+
+The vertices on the graph represent Venmo users and whenever one user pays another user, an edge is formed between the two users.
+
+
+# Rules and Guideliness
+
+The pipelines will process the input file, compute the median and dump it in a file. Following rules is expected by the coordinator:
+
+#### May I use R or other analytics programming languages to solve the challenge?
+Python is used for this challange. The code was tested for both Python 2.7 and Python 3.5. However, Python 3.5 is prefered.
+
+#### What sort of system should I use to run my program on (Windows, Linux, Mac)?
+It was written in MacOS environtment and should work in Linux. A Docker container for this submission is also available (more information below).
+
+#### Can I use pre-built packages, modules, or libraries?
+No "exotic" packages was used. A minimal and Python standard is used (e.g `json`, `datetime`).
+
+#### Will you email me if my code doesn't run?
+The codes were tested using the provided script. Additionally, a Docker container with the exact environtment is also available.
+
+#### Do I need to update the median when the next payment in the file falls outside the 60-second window?
+Any new payments with timestamps > than current maximum timestamps or 60s before current maximum timestamps are accepted. 
+
+#### Should the 60-second window be inclusive or exclusive? 
+Exclusive is used. New payment must be > ( current max timestamps - 60s)
+
+#### Should my graph contain disconnected nodes?
+The graph is pruned everytime the egdes are updated.
+
+####  Should I check if the files in the input directory are text files or non-text files(binary)?
+Only text files with the same format with given input file is accepted.
+
+#### If there are multiple payments within a 60-second window from the same two users, should they be connected twice?
+In this case, the script does not add a new egde; however, it does update the egdes if it's more recent, then followed by pruning procedures and computing the (possibly) new median.
+
+
+# Input and Output
+
+For this challenge, the input a `.txt` file containing at least three information: `actor`, `target` and `created_time` in a json format.
+
+The output is a `.txt` file with each row is the median for every state of the transaction in the input file. 
+
+Generally, to run the script:
+```
+python rolling_median.py input.txt output.txt 
+
+```
+
+To generate a detailed log file, use verbose. The Produced logfile is generally large, and may significantly slow down the process. 
+
+```
+python rolling_median.py input.txt output.txt logfile.txt
+
+```
+
+# Design
+
+### Algorithm
+By analysing the given sample file, the expected graphs are less likely dense (each vertex usually only connected with few other vertex). For this approach, adjency list is a better approach (less space complexity and portentially less time complexity) than other approaches such as adjency matrix.
+
+### Data Structure
+Relatively simple object oriented designs, supported by dictionaries and tuples, are mainly used. This consideration was chosen given that a large (but skinny) data is expected. A relatively simple changes can be added should the data structure changes without sacrifying the performance.
+
+### Time Complexity
+
+Putting aside of the time complexity for Python built-in processes (e.g `dictionary.remove()`, or `sort()`), the time complexity for this approach is O(|V|+|E|).
+Here's the detail:
+
+Time | Procedures | Sub Procedures
+--- | --- | ---
+1   | Add a new node (actor) | 
+    | Add an egde to target | 
+1   | | Check if actor exists  
+1   | | Check timestamps
+V+E | | Check / update existing transaction
+1   | | Add egde 
+V   | Update maximum timestamps |
+E   | Prune egdes |
+V   | Prune vertex |
+    | Update median |
+V+E | | Collect/iterate through graph 
+1   | | Compute median 
+
+Total = V+E + 2V + E + V+E = 4V+3E = V+E
+
+# Docker Package
+
+This submission is also available as a Docker package.
+
+On docker-enabled infrastructures run:
+
+```
+docker run -v /Users/path/to/files:/data kristiyanto/orinocoflow 
+```
+
+The container will process any txt files found in the mounted directory and produce an output with suffix `.OUT`, and exit when finished. 
+
+
---
+++ src/rolling_median.py
@@ -0,0 +1,272 @@
+#!/usr/bin/python3
+
+###########################################################
+# Daniel Kristiyanto, danielkr@uw.edu
+#
+# A graph to find a money transfer transaction in Venmo
+# Submission for Coding Challange by InsightDataScience
+# https://github.com/InsightDataScience/coding-challenge
+# Please refer readme.md for more detailed usage and information
+#
+###########################################################
+
+import os
+import sys
+import json
+import datetime as dt
+from collections import Counter
+
+
+class Graph:
+    def __init__(self, out='output.txt', verbose=False):
+        self.vertices = {}
+        self.edges = {}
+        self.outputFile = out
+        self.median = 0.00
+        self.timeWindow = timeWindow
+        self.verbose = verbose
+    
+    ## Function to add a node. A none instance is required
+    def addNode(self, node):
+        if not isinstance(node, Node): raise ValueError(\
+            'Adding a node error: A node must be a node instance.')
+        if node.actor not in self.vertices: 
+            self.vertices[node.actor] = node
+            if len(self.vertices) == 1: 
+                self.currMaxTime = node.created_time
+                self.curMinTime = self.currMaxTime - dt.timedelta(seconds=self.timeWindow)
+            return True
+        return False
+
+    ## Function to add an egde, if conditions in isValid() met
+    ## Changes trigger graph pruning and rolling median calculation
+    def addTarget(self, node, target, **kwargs):
+        if not isinstance(node, Node): raise ValueError(\
+            'Adding a node error: A node must be a node instance.')
+        if self.isValid(node, target, node.created_time):
+            self.vertices[node.actor].addTarget((target, node.created_time))
+            self.currMaxTime = self.updateMaxTime()
+            self.curMinTime = self.currMaxTime - dt.timedelta(seconds=self.timeWindow)
+            self.prune()
+            self.updateMedian()
+            return True
+        else:
+            self.prune()
+            if self.verbose: print("Rejected: " + node.actor + ', ' \
+            + node.created_time.strftime(timeFormat))
+            return False
+
+    ## Filtering incoming edges/transactions
+    ## e.g: timestamps, if actor is valid,
+    ## or if transactions is already existed (hence, replace it if newer)
+    def isValid(self, node, target, created_time):
+        if node.actor not in self.vertices: return False
+        if node.created_time < self.curMinTime: return False
+        if (target in [n[0] for n in self.vertices[node.actor].getTargets()]):
+            return self.vertices[node.actor].replaceTarget(target, created_time)
+        elif(target in self.vertices):
+            return self.vertices[target].replaceTarget(node.actor, created_time)
+        else:
+            return True
+
+    ## Returning current time window for the graph (min and max timestamps)    
+    def getWindow(self):
+        return (self.curMinTime, self.currMaxTime)
+    
+    ## Scan through the graph to get the maximum timestamps
+    def updateMaxTime(self):
+        for v in list(self.vertices):
+            if len(v) < 1 : return self.currMaxTime
+            if self.currMaxTime < self.vertices[v].getMaxTime():
+                self.currMaxTime = self.vertices[v].getMaxTime()
+        return self.currMaxTime
+    
+    ## A static call returning current maximum timstamp
+    def getMaxTime(self):
+        if not self.currMaxTime: self.updateMaxTime()
+        return self.currMaxTime
+    
+    ## Scan through the graph to compute the rolling median 
+    def updateMedian(self):
+        egdeList = list()
+        for v in self.vertices:
+            egdeList.extend([n[0] for n in self.vertices[v].getTargets()])
+            egdeList.extend([v]*len(self.vertices[v].getTargets()))
+        unique = Counter(egdeList)
+        self.edges = unique          
+        unique = sorted([n for n in list(unique.values())])
+        self.median = median(unique)
+        return(self.median)
+
+    ## Static call retruning current roll median, and save it to output file
+    def getMedian(self, write=True, display=False):
+        if write: 
+            with open(self.outputFile, "a") as o: o.write('{0:.2f} \n'.format(self.median))
+        if self.verbose:
+            print(self.median)
+        return self.median
+
+    ## Returning list of the current egdes (mainly for log purposes)
+    def getEgdeList(self):
+        return self.edges
+    
+    ## Pruning the graph
+    def prune(self):
+        for v in list(self.vertices):
+            self.vertices[v].updateTargets(self.getWindow()[0])
+            if not self.vertices[v].getTargets() : 
+                del self.vertices[v]
+        return self.updateMaxTime()
+
+    ## Returning the graph connections for log purposes
+    def printGraph(self):
+        txt = ""
+        for v in self.vertices:
+            txt = txt + ('Actor: {} \n Targets: {} \n'.format(v, \
+            [(i[0], i[1].strftime("%I:%M:%S")) for i in self.vertices[v].targets]))
+        return txt
+                
+        
+            
+class Node:
+    ## By default, a vertex (node) does not have any egdes/target
+    ## Until an egde is spesifically added. A node can have multiple egdes
+    
+    def __init__(self, actor, created_time, target, **kwargs):
+        self.targets = list()
+        self.initialTarget = target
+        self.actor = actor
+        self.totalTargets = 0
+        self.created_time = dt.datetime.strptime(created_time, timeFormat)
+        self.timewindow = timeWindow
+        self.nodeMaxTime = self.created_time
+    
+    ## Function to add a new egde
+    def addTarget(self, target):
+        self.targets.append(target)
+        if target[1] > self.nodeMaxTime: self.nodeMaxTime = target[1]
+    
+    ## If an egde already existed, and the incoming has a newer timstamps
+    ## delete the egde so the graph can proceed adding the new egde
+    def replaceTarget(self, target, created_time):
+        for t in self.targets:
+            if t[0] == target and t[1] < created_time:
+                self.targets.remove(t)
+                return True
+            else:
+                return False
+            
+    ## Function to prune expiring egdes
+    def updateTargets(self, minTime):
+        newTargets = list()
+        newTimes = list()
+        for t in self.targets:
+            if t[1] > minTime: 
+                newTargets.append(t)
+                newTimes.append(t[1])
+        self.targets = newTargets
+        if not self.nodeMaxTime: self.nodeMaxTime  = max(newTimes)
+    
+    ## A static call returning the list of this node's connection/egdes
+    def getTargets(self):
+        return self.targets
+    
+    ## A static call returning maximum timestamps for each egdes
+    def getMaxTime(self):
+        return self.created_time if not self.targets else self.nodeMaxTime
+
+    
+## Function to compute a median given a list    
+def median(vector):
+    if not isinstance(vector, list): raise ValueError('Input must be a list.')
+    vector = sorted(vector)
+    a = len(vector)
+    b = a - 1
+    return (vector[a//2] + vector[b//2]) / 2.0
+
+
+## Function to record a detailed state of the current graph to a log file. 
+def logItems(graph, write=True, file='log.txt', verbose=False):
+    if not isinstance(graph, Graph): raise ValueError( \
+    'Log error: Log input must be a graph instance.')
+    Min = graph.getWindow()[0].strftime("%I:%M:%S")
+    Max = graph.getWindow()[1].strftime("%I:%M:%S")
+    txt = "MEDIAN: {} \n Sorted Egdes: {} \n Edges: \n {} \
+        \n\n Time Window: {} {} \n\n Transactions: \n {} \
+        ------- \n \n".format( \
+        graph.getMedian(write=False, display=False), \
+        sorted([n for n in list(graph.getEgdeList().values())]), \
+        graph.getEgdeList(), \
+        Min, Max, \
+        graph.printGraph()) 
+    if verbose: print(txt)
+    if write: 
+        with open(file, "a") as o: o.write(txt)
+
+## Making sure the specified files are readable and or writeable            
+def filesLookup(inputFile, logFile='log.txt', outputFile='output.txt'):
+    if not os.path.exists(inputFile): 
+        raise ValueError('Input file: {} not found.'.format(inputFile))
+        
+        try:
+            json = json.loads(open(inputFile, 'r').readline())
+            if ('actor', 'created_time', 'target') in list(json.keys()): pass
+        except:
+            raise ValueError('Input file: {} does not have required json fields.'.format(inputFile))
+
+    try:
+        open(logFile, 'w').close()
+        return True
+    except:
+        raise ValueError('Unable to write output files.')
+        
+    try:
+        open(outputFile, 'w').close()
+    except:
+        pass
+
+def main():
+    global timeFormat
+    global timeWindow
+    
+    ## The following global variables must be defined
+    ## Time Window is the cuttoff time in seconds
+    timeFormat = '%Y-%m-%dT%H:%M:%S%fZ'
+    timeWindow = 60
+    
+    ## Reading through the arguments
+    inputFile = sys.argv[1].strip() if len(sys.argv)>1 else 'venmo-trans.txt'
+    outputFile = sys.argv[2].strip() if len(sys.argv)>2 else 'output.txt' 
+    logFile = sys.argv[3].strip() if len(sys.argv)>3 else None
+
+    
+    ## Get the files ready
+    filesLookup(outputFile=outputFile, inputFile=inputFile) 
+    
+    ## This is the canvas
+    g = Graph(out=outputFile, verbose=False)
+    
+    ## Reading through the input file
+    with open(inputFile) as f:
+        for line in f:
+            entry = json.loads(line)
+            
+            # Skip incomplete entries
+            if(entry['created_time'] and entry['target'] and entry['actor']):
+                
+                # 1) Convert json to a node instance
+                newNode = Node(**entry)
+                
+                # 2) Add the node to the graph
+                g.addNode(newNode)
+                
+                # 3) Add an egde to the node
+                g.addTarget(newNode, **entry)
+                
+                # 4) Write the median to a File 
+                g.getMedian(write=True)
+                
+                # 5) If asked (nicely), log the current graph state
+                if logFile: logItems(g, write=True, verbose=False)            
+  
+if __name__ == "__main__": main()
